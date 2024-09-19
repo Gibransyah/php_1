20c9422d1ab87f3cd7fc43c791b739950b2b230a
gibransyah <gibransyahagung25@gmail.com>
2024-09-19 23:49:08 +0700

    first commit

---
+++ Gibransyah.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> 
+        
+    PHP | Gibransyah
+
+    </title>
+</head>
+<body>
+
+<!-- memanggil layout header -->
+<?php include "layout/header.html" ?>
+    
+<main>
+
+<center>
+
+<i>   
+Lorem Ipsum dolor, sit amet consectetur adipisicing elit, Esse, natus
+</i>
+
+</center>
+
+</main>
+
+<!-- memanggil layout footer -->
+<?php include "layout/footer.html" ?>
+
+</body>
+</html>
